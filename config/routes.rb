48d6391ae6dfc9436cf8da05b9871a403a778cf3
Sam Nixa <spiznasm@gmail.com>
2017-10-03 21:11:18 +0000

    Working log in and out

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,15 @@
 Rails.application.routes.draw do
+  
+  root 'students#index'
+  
+  get '/login', to: 'sessions#new'
+  post '/login', to: 'sessions#create'
+  delete '/logout', to: 'sessions#destroy'
+  
+  get '/signup', to: 'users#new'
+  post '/signup', to: 'users#create'
+
   resources :students
+  
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
