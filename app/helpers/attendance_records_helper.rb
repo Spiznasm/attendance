f83e2582ab8aff3ddb4cbef350e4f8aad7ee4b66
Sam Nixa <spiznasm@gmail.com>
2017-10-09 21:21:49 +0000

    Added blank option for time_out

--- app/helpers/attendance_records_helper.rb
+++ app/helpers/attendance_records_helper.rb
@@ -1,7 +1,11 @@
 module AttendanceRecordsHelper
   
   def hour_calc(record)
-    ((record.time_out - record.time_in)/60 - (record.break.nil? ? 0 : record.break))/60*10.to_i / 10
+    if record.time_out.blank?
+      0
+    else
+      ((record.time_out - record.time_in)/60 - (record.break.nil? ? 0 : record.break))/60*10.to_i / 10
+    end
   end
   
 end
