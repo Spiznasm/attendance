47011d09657fd76d84f0e595aaf84ff0cc34b22b
Sam Nixa <spiznasm@gmail.com>
2017-10-11 16:17:31 +0000

    Display current day's attendance on student index page

--- app/models/student.rb
+++ app/models/student.rb
@@ -2,4 +2,5 @@ class Student < ApplicationRecord
   has_many :attendance_records
   validates :name, presence: true
   validates :genesis_id, presence: true
+  accepts_nested_attributes_for :attendance_records
 end
